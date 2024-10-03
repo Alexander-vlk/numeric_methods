a3a4ac38d96ff1f84ee605e949adeedf8e1f0d25
alex_barmen <www.lad282@gmail.com>
2024-10-03 23:00:10 +0300

    Added functionality to choose numeric method via console argument

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 from pathlib import Path
 
 
@@ -5,12 +8,30 @@ from running_method import RunningMethod
 
 
 if __name__ == '__main__':
-
+    
     BASE_DIR = Path(__file__).resolve().parent
 
     INPUT_FILE = BASE_DIR.joinpath('io_data/INPUT.txt')
     OUTPUT_FILE = BASE_DIR.joinpath('io_data/OUTPUT.txt')
 
-    method = RunningMethod(input_file=INPUT_FILE, output_file=OUTPUT_FILE)
+    called_method = sys.argv[1]
+
+    help_commands = ('--help', 'help', '--h', )
+
+    numeric_methods = {
+        'running_method': RunningMethod,
+    }
+
+    if called_method in help_commands:
+        print('Available numeric methods:')
+        print('\n'.join(numeric_method for numeric_method in numeric_methods.keys()))
+        exit()
+    
+    if not numeric_methods.get(called_method):
+        raise ModuleNotFoundError(
+            f'No such numeric method {called_method}. Type "{help_commands[0]}" to see allowed methods.'
+            )
+
+    method = numeric_methods.get(called_method)(input_file=INPUT_FILE, output_file=OUTPUT_FILE)
 
     method.write_file()
