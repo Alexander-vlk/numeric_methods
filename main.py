f5ac80bd443e75a1d234dab67e4a6411bf1a872f
alex_barmen <www.lad282@gmail.com>
2024-10-03 22:35:26 +0300

    Added logic to rmain to run running_method

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from pathlib import Path
+
+
+from running_method import RunningMethod
+
+
 if __name__ == '__main__':
-    pass
+
+    BASE_DIR = Path(__file__).resolve().parent
+
+    INPUT_FILE = BASE_DIR.joinpath('io_data/INPUT.txt')
+    OUTPUT_FILE = BASE_DIR.joinpath('io_data/OUTPUT.txt')
+
+    method = RunningMethod(input_file=INPUT_FILE, output_file=OUTPUT_FILE)
+
+    method.write_file()
