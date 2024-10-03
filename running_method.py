e7819dbd59cb012b33e8a37dbe3b103f72274f31
alex_barmen <www.lad282@gmail.com>
2024-10-03 20:42:22 +0300

    Added NumericMethod class and realized mehtods read_file and write_file

---
+++ running_method.py
@@ -0,0 +1,9 @@
+from numeric_method import NumericMethod
+
+
+class RunningMethod(NumericMethod):
+    def __init__(self, *, input_file, output_file):
+        super().__init__(input_file, output_file)
+
+    def solve(self):
+        pass
