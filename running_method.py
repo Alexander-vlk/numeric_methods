73c272244fd7f820c81d4dcf36d34066796349d2
alex_barmen <www.lad282@gmail.com>
2024-10-04 20:34:25 +0300

    Added: - _is_correct method - _is_sustainable method - __ROUND_VALUE const finalized: - _solve method

--- running_method.py
+++ running_method.py
@@ -3,6 +3,7 @@ from numeric_method import NumericMethod
 
 class RunningMethod(NumericMethod):
     __EPSILON = 10e-6
+    __ROUND_VALUE = 6
 
     def __init__(self, *, input_file, output_file):
         super().__init__(input_file, output_file)
@@ -19,12 +20,20 @@ class RunningMethod(NumericMethod):
         }
         return coefficients.values()
     
+    def _is_correct(self, denominator):
+        if abs(denominator) < self.__EPSILON:
+            raise ZeroDivisionError(
+                f'(Fatal): It seems like your data are incorrect: {denominator}'
+                )
+    
+    def _is_sustainable(self,*, deltas):
+        return all(abs(deltas[i]) > self.__EPSILON for i in range(self.n))
+    
     def _get_run_factors(self):
         b, c, d = self._get_coefficients()
         r = self.data.get('free_factors')
 
-        if abs(c[0]) < self.__EPSILON:
-            raise ZeroDivisionError(f'It seems like your data are incorrect: c1 = {c[0]}')
+        self._is_correct(c[0])
         
         run_factors = {
             'delta': [-d[0]/c[0]],
@@ -33,11 +42,16 @@ class RunningMethod(NumericMethod):
         for i in range(1, self.n-1):
             last_delta = run_factors['delta'][i-1]
             last_lambda = run_factors['lambda'][i-1]
+
+            denominator = c[i] + b[i] * last_delta
+
+            self._is_correct(denominator)
+            
             run_factors['delta'].append(
-                (- d[i] / (c[i] + b[i] * last_delta))
+                (- d[i] / denominator)
             )
             run_factors['lambda'].append(
-                (r[i] - b[i] * last_lambda) / (c[i] + b[i]*last_delta)
+                (r[i] - b[i] * last_lambda) / denominator
             )
         
         run_factors['delta'].append(0)
@@ -51,12 +65,25 @@ class RunningMethod(NumericMethod):
         
     def _solve(self):
         delta_run_factors, lambda_run_factors = self._get_run_factors()
-        
-        # TODO: Make solvation: direct run and reverse run
+        print('(Success): Solution is correct')
 
         solution = [0] * self.n
+        
+        current_x = lambda_run_factors[self.n-1] 
+        solution[self.n-1] = round(current_x, self.__ROUND_VALUE)
+        
+        if not self._is_sustainable(deltas=delta_run_factors):
+            print('(Warning): Solution is unsustainable')
+
+        for i in range(self.n-1, -1, -1):
+            previous_x = delta_run_factors[i] * current_x + lambda_run_factors[i]
+            solution[i] = round(previous_x, self.__ROUND_VALUE)
+
+            current_x = previous_x
+
         return solution
 
     def write_file(self):
         self.solution = self._solve()
         return super().write_file()
+    
