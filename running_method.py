60c6a2a6fbd93331764404bacba5365e2b39a37a
alex_barmen <www.lad282@gmail.com>
2024-10-20 18:09:29 +0300

    feat: added logging to running method

--- running_method.py
+++ running_method.py
@@ -1,4 +1,9 @@
+import logging
+
 from numeric_method import NumericMethod
+
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 
 class RunningMethod(NumericMethod):
@@ -9,7 +14,7 @@ class RunningMethod(NumericMethod):
         super().__init__(input_file, output_file)
         self.read_file()
         self.n = self.data.get('dimension')
-
+        self.log = logging.getLogger(__name__)
 
     def _get_coefficients(self):
         matrix = self.data.get('matrix')
@@ -22,11 +27,11 @@ class RunningMethod(NumericMethod):
     
     def _is_correct(self, denominator):
         if abs(denominator) < self.__EPSILON:
-            raise ZeroDivisionError(
-                f'(Fatal): It seems like your data are incorrect: {denominator}'
-                )
+            self.log.exception(f'It seems like your data are incorrect: {denominator}')
+            exit()
     
     def _is_sustainable(self,*, deltas):
+        self.log.info(f'deltas {deltas}')
         return all(abs(deltas[i]) > self.__EPSILON for i in range(self.n))
     
     def _get_run_factors(self):
@@ -39,11 +44,15 @@ class RunningMethod(NumericMethod):
             'delta': [-d[0]/c[0]],
             'lambda': [r[0]/c[0]],
         }
+        self.log.info(f'delta 1: {run_factors['delta'][0]}')
+        self.log.info(f'lambda 1: {run_factors['lambda'][0]}')
+
         for i in range(1, self.n-1):
             last_delta = run_factors['delta'][i-1]
             last_lambda = run_factors['lambda'][i-1]
 
             denominator = c[i] + b[i] * last_delta
+            self.log.info(f'denominator {i}: {denominator}')
 
             self._is_correct(denominator)
             
@@ -53,6 +62,9 @@ class RunningMethod(NumericMethod):
             run_factors['lambda'].append(
                 (r[i] - b[i] * last_lambda) / denominator
             )
+
+            self.log.info(f'delta {i}: {run_factors['delta'][i]}')
+            self.log.info(f'lambda {i}: {run_factors['lambda'][i]}')
         
         run_factors['delta'].append(0)
 
@@ -65,7 +77,7 @@ class RunningMethod(NumericMethod):
         
     def _solve(self):
         delta_run_factors, lambda_run_factors = self._get_run_factors()
-        print('(Success): Solution is correct')
+        self.log.info('Solution is correct')
 
         solution = [0] * self.n
         
@@ -73,7 +85,9 @@ class RunningMethod(NumericMethod):
         solution[self.n-1] = round(current_x, self.__ROUND_VALUE)
         
         if not self._is_sustainable(deltas=delta_run_factors):
-            print('(Warning): Solution is unsustainable')
+            self.log.warning('Solution is unsustainable')
+        else:
+            self.log.info('Solution is sustainable')
 
         for i in range(self.n-1, -1, -1):
             previous_x = delta_run_factors[i] * current_x + lambda_run_factors[i]
